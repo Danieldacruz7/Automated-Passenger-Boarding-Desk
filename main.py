3af7210284dc5a6ca5257217cc3a5cfc031cd33c
danieldacruz7 <danieldacruz1995@hotmail.co.za>
2022-11-07 05:51:52 +0200

    SDK cleints added.

--- main.py
+++ main.py
@@ -22,15 +22,41 @@ from azure.cognitiveservices.vision.customvision.training import CustomVisionTra
 from azure.cognitiveservices.vision.customvision.prediction import CustomVisionPredictionClient
 from azure.cognitiveservices.vision.customvision.training.models import ImageFileCreateBatch, ImageFileCreateEntry, Region
 from msrest.authentication import ApiKeyCredentials
-
+from azure.core.exceptions import ResourceNotFoundError
+from azure.ai.formrecognizer import FormTrainingClient
 load_dotenv()
 
 CONFIG = {
     'SUBSCRIPTION_KEY': os.getenv("SUBSCRIPTION_KEY"),
     'LOCATION': 'trial',
     'ACCOUNT_ID': os.getenv("ACCOUNT_ID"), 
+
     'FACIAL_RECOGNITION_ENDPOINT': os.getenv('AZURE_FACIAL_RECOGNIZER_ENDPOINT'), 
     'FACIAL_RECOGNITION_KEY': os.getenv('AZURE_FACIAL_RECOGNIZER_KEY'), 
+
     'FORM_RECOGNITION_ENDPOINT': os.getenv('AZURE_FORM_RECOGNIZER_ENDPOINT'), 
-    'FORM_RECOGNITION_KEY': os.getenv('AZURE_FORM_RECOGNIZER_KEY')
+    'FORM_RECOGNITION_KEY': os.getenv('AZURE_FORM_RECOGNIZER_KEY'),
+
+    'OBJECT_DETECTION_TRAINING_ENDPOINT' : os.getenv('OBJECT_DETECTION_TRAINING_ENDPOINT'), 
+    'OBJECT_DETECTION_TRAINING_KEY' : os.getenv('OBJECT_DETECTION_TRAINING_KEY'),
+    'OBJECT_DETECTION_TRAINING_RESOURCE_ID' : os.getenv('OBJECT_DETECTION_TRAINING_RESOURCE_ID'),
+
+    'OBJECT_DETECTION_PREDICTION_ENDPOINT' : os.getenv('OBJECT_DETECTION_PREDICTION_ENDPOINT'),
+    'OBJECT_DETECTION_PREDICTION_KEY' : os.getenv('OBJECT_DETECTION_PREDICTION_KEY'),
+    'OBJECT_DETECTION_PREDICTION_RESOURCE_ID' : os.getenv('OBJECT_DETECTION_PREDICTION_RESOURCE_ID')
 }
+
+form_recognizer_client = FormRecognizerClient(endpoint=CONFIG['FORM_RECOGNIZER_ENDPOINT'], credential=AzureKeyCredential(CONFIG['FORM_RECOGNIZER_KEY']))
+form_training_client = FormTrainingClient(endpoint=CONFIG['FORM_RECOGNITION_ENDPOINT'], credential=AzureKeyCredential(CONFIG['FORM_RECOGNIZER_KEY']))
+face_client = FaceClient(CONFIG['FACIAL_RECOGNITION_ENDPOINT'], CognitiveServicesCredentials(CONFIG['FACIAL_RECOGNITION_ENDPOINT']))
+training_credentials = ApiKeyCredentials(in_headers={"Training-key": CONFIG['OBJECT_DETECTION_TRAINING_KEY']})
+trainer = CustomVisionTrainingClient(CONFIG['OBJECT_DETECTION_TRAINING_ENDPOINT'], training_credentials)
+
+prediction_credentials = ApiKeyCredentials(in_headers={"Prediction-key": CONFIG['OBJECT_DETECTION_PREDICTION_KEY']})
+predictor = CustomVisionPredictionClient(CONFIG['OBJECT_DETECTION_PREDICTION_ENDPOINT'], prediction_credentials)
+
+video_analysis = VideoIndexer(
+    vi_location=CONFIG['LOCATION'],
+    vi_account_id=CONFIG['ACCOUNT_ID'], 
+    vi_subscription_key=CONFIG['SUBSCRIPTION_KEY']
+)
